fc5e14d2f018060c6d02b36ee8cd3dbaa2f776d9
Khepu <makris.giwrgos1234@gmail.com>
2023-02-07 14:14:32 +0200

    Feature: Basic speech recognition

--- Warcry/Library.fs
+++ Warcry/Library.fs
@@ -1,5 +1,7 @@
 ﻿namespace Warcry
 
+open System
+open System.Globalization
 open System.Speech.Recognition
 open Warcry.Order
 
@@ -12,3 +14,19 @@ module Core =
             List.iter (fun order -> builder.Append(grammar order)) orders
             builder
         | Choice choices -> GrammarBuilder(Choices(List.map grammar choices |> Array.ofList))
+    
+    let recognitionHandler (speechRecognizedEventArgs: SpeechRecognizedEventArgs) = 
+        let result = speechRecognizedEventArgs.Result
+
+        if result.Confidence > 0.f then
+            Console.WriteLine $"[f{result.Confidence}] {result.Text}"
+    
+    let main =
+        let speechRecognition = new SpeechRecognitionEngine(CultureInfo("en-US"))
+        try
+            speechRecognition.SetInputToDefaultAudioDevice()
+        with _ -> failwith "Failed to find default audio device!"
+
+        speechRecognition.LoadGrammar(Grammar(grammar orders))
+        speechRecognition.SpeechRecognized.Add(recognitionHandler)
+        speechRecognition.RecognizeAsync(RecognizeMode.Multiple)
